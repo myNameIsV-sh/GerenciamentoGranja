249d623c1f808e2484f821a9ff95291b6254537a
Filipeoliveirap <filipejpo@gmail.com>
2026-02-23 17:09:47 -0300

    feat(db): adicionar conexão com banco de dados PostgreSQL e teste de conexão

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,28 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_restful import Api
+from dotenv import load_dotenv
+from sqlalchemy import text
 from app.services.logging_service import setup_logger
 
 logger = setup_logger()
+
+load_dotenv()
+
+db = SQLAlchemy()
+ma = Marshmallow()
+api = Api()
+
+
+def test_db_connection():
+    try:
+        with db.engine.connect() as conn:
+            result = conn.execute(text("SELECT 1"))
+            print("Conexão OK")
+    except Exception as e:
+        print("Erro na conexão:", e)
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +32,24 @@ def create_app():
 
     app.register_blueprint(v1_bp)
 
+    db_user = os.getenv('DB_USER')
+    db_password = os.getenv('DB_PASSWORD')
+    db_host = os.getenv('DB_HOST')
+    db_port = os.getenv('DB_PORT')
+    db_name = os.getenv('DB_NAME')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    ma.init_app(app)
+    api.init_app(app)
+
+    with app.app_context():
+        test_db_connection() 
+
     logger.info("Aplicação Flask iniciada com Flask-RESTful")
+
+    
 
     return app
