934e0eeb0d0c114e595a24e141e610b58741f231
Victor Gustavo <victor.gustavo.c@proton.me>
2026-02-26 03:27:20 -0300

    feat: instanciação dos serviços e registro dos endpoints

--- app/resources/v1/__init__.py
+++ app/resources/v1/__init__.py
@@ -1,8 +1,51 @@
 from flask import Blueprint
 from flask_restful import Api
-from .resource_example import ResourceExample
+
+from app.repositories.LoteRepository import LoteRepository
+from app.repositories.GalpaoRepository import GalpaoRepository
+
+from app.services.LoteService import LoteService
+from app.services.GalpaoService import GalpaoService
+
+from app.resources.v1.LoteResource import LoteResource
+from app.resources.v1.GalpaoResource import GalpaoResource
 
 v1_bp = Blueprint('v1', __name__, url_prefix='/api/v1')
 api = Api(v1_bp)
 
-api.add_resource(ResourceExample, '/example')
+# ==========================================
+# INJEÇÃO DE DEPENDÊNCIAS
+# ==========================================
+
+# A. Instanciar os repositórios (acesso a dados)
+lote_repo = LoteRepository()
+galpao_repo = GalpaoRepository()
+
+# B. Instanciar os serviços base
+galpao_service = GalpaoService(galpao_repository=galpao_repo)
+
+# C. Instanciar o serviço orquestrador 
+lote_service = LoteService(
+    lote_repository=lote_repo, 
+    galpao_service=galpao_service
+)
+
+# ==========================================
+# REGISTRO DAS ROTAS (Endpoints)
+# ==========================================
+
+# Rotas para gerir os Lotes
+api.add_resource(
+    LoteResource, 
+    '/lotes',
+    '/lotes/<int:id_lote>',
+    resource_class_kwargs={'lote_service': lote_service}
+)
+
+# Rotas para gerir a infraestrutura dos Galpões
+api.add_resource(
+    GalpaoResource, 
+    '/galpoes',
+    '/galpoes/<int:id_galpao>',
+    resource_class_kwargs={'galpao_service': galpao_service}
+)
