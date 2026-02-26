8690c9f6dceab91ed37c4affc53a40b6431ba3ab
Victor Gustavo <victor.gustavo.c@proton.me>
2026-02-25 22:43:13 -0300

    feat: definição das regras do Galpão

--- app/services/GalpaoService.py
+++ app/services/GalpaoService.py
@@ -1,2 +1,9 @@
 class GalpaoService:
-    pass
+    def __init__(self, galpao_repository):
+        self.galpao_repository = galpao_repository
+
+    def obter_galpao_por_id(self, id_galpao: int):
+        galpao = self.galpao_repository.get_by_id(id_galpao)
+        if not galpao:
+            raise ValueError(f"Galpão com ID {id_galpao} não foi encontrado.")
+        return galpao
