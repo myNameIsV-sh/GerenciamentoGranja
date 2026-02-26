2382ccb061ce439cc855d7ad37c6c1060ead07f5
Alice Tavares <doraaventur3@gmail.com>
2026-02-26 00:52:06 -0300

    test: valida status inválidos

--- app/tests/TestGalpaoService.py
+++ app/tests/TestGalpaoService.py
@@ -14,3 +14,14 @@ class TestGalpaoService:
     def test_erro_not_found(self, galpao_service, galpao_repo_mock):
         # Simula o banco não encontrando o galpão
         galpao_repo_mock.get_by_id.return_value = None
+        
+        with pytest.raises(ValueError, match="não foi encontrado"):
+            galpao_service.obter_galpao_por_id(999)
+            
+    def test_status_invalido(self, galpao_service, galpao_repo_mock):
+        # Simula um galpão existente
+        galpao_repo_mock.get_by_id.return_value = Mock()
+        
+        # Garante que o ValueError é levantado para status não permitidos
+        with pytest.raises(ValueError, match="Status inválido"):
+            galpao_service.atualizar_status_ocupacao(1, "Pegando Fogo")
