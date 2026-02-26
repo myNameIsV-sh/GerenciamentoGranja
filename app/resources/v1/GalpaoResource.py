d7fbccd12cd8c310c8a40d113fe6498235709618
Victor Gustavo <victor.gustavo.c@proton.me>
2026-02-26 02:51:01 -0300

    feat: construtor para o GalpaoResource

--- app/resources/v1/GalpaoResource.py
+++ app/resources/v1/GalpaoResource.py
@@ -1,6 +1,16 @@
+from flask import request
 from flask_restful import Resource
+from app.schemas.GalpaoSchema import GalpaoSchema
+from app.services.logging_service import setup_logger
+
+logger = setup_logger("GalpaoResource")
 
 class GalpaoResource(Resource):
+    def __init__(self, **kwargs):
+        self.galpao_service = kwargs.get('galpao_service')
+        self.galpao_schema = GalpaoSchema()
+        self.galpoes_schema = GalpaoSchema(many=True)
+
     def get(self):
         pass
     def post(self):
