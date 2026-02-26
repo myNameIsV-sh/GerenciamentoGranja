8bcc38ff3fd527d696240106921ffc9dbf58e6fe
Victor Gustavo <victor.gustavo.c@proton.me>
2026-02-26 03:04:12 -0300

    fix: importações do LoteSchema

--- app/schemas/LoteSchema.py
+++ app/schemas/LoteSchema.py
@@ -1,11 +1,11 @@
-from marshmallow import fields, validate
+from marshmallow import Schema, fields, validate
 
 class LoteSchema(Schema):
     id_lote = fields.Int(dump_only=True)
     id_galpao = fields.Int(required=True)
-    data_alojamento = fields.Date(required=True) # ISO8601 por padrão (YYYY-MM-DD)
+    data_alojamento = fields.Date(required=True)  # ISO8601 por padrão (YYYY-MM-DD)
     status = fields.Str(validate=validate.OneOf(["Ativo", "Finalizado"]))
-    
+
     quantidade_inicial_aves = fields.Int(required=True, validate=validate.Range(min=1))
     ultimo_peso_g = fields.Float(validate=validate.Range(min=0))
     consumo_total_racao_kg = fields.Float(validate=validate.Range(min=0))
@@ -17,3 +17,11 @@ class LoteSchema(Schema):
     def get_semana_atual(self, obj):
         # Chama a property definida na classe Lote
         return obj.calcular_idade_em_semanas
+
+
+# <-- NOVO: O Schema específico para a rota de consumo que havíamos planejado
+class RegistroConsumoSchema(Schema):
+    racao_consumida_kg = fields.Float(
+        required=True,
+        validate=validate.Range(min=0.1, error="A quantidade de ração deve ser maior que zero.")
+    )
