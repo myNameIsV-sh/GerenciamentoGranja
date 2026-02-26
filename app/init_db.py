9deef26e4ffe6845416246c8670d28bc83707131
Victor Gustavo <victor.gustavo.c@proton.me>
2026-02-26 14:14:05 -0300

    fix: problema na criação de tabelas

--- app/init_db.py
+++ app/init_db.py
@@ -1,16 +1,25 @@
-from app import create_app, db, setup_logger
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import create_app, db
+from app.services.logging_service import setup_logger
+
 from app.models.Galpao import Galpao
 from app.models.Lote import Lote
-from app.services import logging_service
 
-logger = setup_logger()
+logger = setup_logger("init_db")
 
 app = create_app()
 
 def inicializar_banco():
     with app.app_context():
-        db.create_all()
-        logger("Tabelas 'galpao' e 'lote' criadas com sucesso no banco de dados!")
+        try:
+            logger.info("Iniciando a criação das tabelas...")
+            # O create_all olha para a Base que Galpao e Lote herdaram
+            db.create_all()
+            logger.info("Tabelas 'galpao' e 'lote' criadas com sucesso no banco de dados!")
+        except Exception as e:
+            logger.error(f"Erro ao criar tabelas: {str(e)}")
 
 if __name__ == '__main__':
     inicializar_banco()
