57e225948139cf2227da17ca47d50461c3cebf02
Victor Gustavo <victor.gustavo.c@proton.me>
2026-02-26 03:03:07 -0300

    fix: importações do GalpaoSchema

--- app/schemas/GalpaoSchema.py
+++ app/schemas/GalpaoSchema.py
@@ -1,12 +1,11 @@
-from marshmallow import fields, validate
+from marshmallow import Schema, fields, validate
 
 class GalpaoSchema(Schema):
     id_galpao = fields.Int(dump_only=True)
     identificacao = fields.Str(required=True, validate=validate.Length(min=1))
-    # Validação baseada nos status permitidos no GalpaoService
+
     status = fields.Str(validate=validate.OneOf(["Livre", "Ocupado", "Em Manutenção"]))
     temperatura_atual = fields.Int(allow_none=True)
-    
-    # Marshmallow trata a conversão de string para objeto time automaticamente
+
     horario_religamento_luzes = fields.Time(format="%H:%M", allow_none=True)
     horario_desligamento_luzes = fields.Time(format="%H:%M", allow_none=True)
