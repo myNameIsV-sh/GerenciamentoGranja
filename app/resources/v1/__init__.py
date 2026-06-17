b081c7220214e67240b1ea99648da13186deeba8
Victor Gustavo <victor.gustavo.c@proton.me>
2026-06-17 09:39:53 -0300

    feat(redis): injeção de dependência do cache para o galpão

--- app/resources/v1/__init__.py
+++ app/resources/v1/__init__.py
@@ -4,6 +4,7 @@ from flask_restful import Api
 from app.repositories.LoteRepository import LoteRepository
 from app.repositories.LoteRepositoryCached import LoteRepositoryCached
 from app.repositories.GalpaoRepository import GalpaoRepository
+from app.repositories.GalpaoRepositoryCached import GalpaoRepositoryCached
 
 from app.services.LoteService import LoteService
 from app.services.GalpaoService import GalpaoService
@@ -22,9 +23,10 @@ api = Api(v1_bp)
 lote_repo = LoteRepository()
 lote_repo_cached = LoteRepositoryCached(repo=lote_repo)
 galpao_repo = GalpaoRepository()
+galpao_repo_cached = GalpaoRepositoryCached(repo=galpao_repo)
 
 # B. Instanciar os serviços base
-galpao_service = GalpaoService(galpao_repository=galpao_repo)
+galpao_service = GalpaoService(galpao_repository=galpao_repo_cached)
 
 # C. Instanciar o serviço orquestrador 
 lote_service = LoteService(
